4e5a043c4cb0c8b14bdedba3338000636a40904b
sampuri <samarthpuri@email.arizona.edu>
2019-02-20 13:36:15 -0700

    push

---
+++ Assets/Scripts/DifficultySettings.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySettings : MonoBehaviour {
+
+    private const int DIFFICULTY_LEVEL_EASY = 0;
+    private const int DIFFICULTY_LEVEL_NORMAL = 1;
+    private const int DIFFICULTY_LEVEL_HARD = 2;
+
+    public static DifficultySettings instance;
+
+    public static float cameraScrollSpeed;
+    public static float playerSpeed;
+    public static float speedupTimer;
+    public static GameObject tile;
+
+    public GameObject[] tilePrefabs;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        int difficultyLevel = DIFFICULTY_LEVEL_NORMAL;
+        switch (difficultyLevel)
+        {
+            case DIFFICULTY_LEVEL_EASY:
+                speedupTimer = 30.0f;
+                break;
+            case DIFFICULTY_LEVEL_NORMAL:
+                speedupTimer = 15.0f;
+                break;
+            case DIFFICULTY_LEVEL_HARD:
+                speedupTimer = 5.0f;
+                break;
+            default:
+                Debug.LogError("Invalid difficulty level: " + difficultyLevel);
+                break;
+        }
+
+        tile = tilePrefabs[difficultyLevel];
+    }
+}
---
+++ Assets/Scripts/QuitButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButtonScript : MonoBehaviour {
+
+    public void OnQuitButtonClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
